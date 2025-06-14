57a68660799476fe4df10201009d01c717998a59
Igor Molchanov <66907532+meg4cyberc4t@users.noreply.github.com>
2025-06-14 12:26:48 +0300

    _

--- source/backend/control_flow_builder.cc
+++ source/backend/control_flow_builder.cc
@@ -10,34 +10,47 @@ namespace backend
 ssa_position control_flow_builder::add_expression(
     const frontend::expression& expr)
 {
+  ssa_position position;
   if (auto expr_ptr = dynamic_cast<const frontend::binary_expression*>(&expr)) {
-    return add_expression(*expr_ptr);
+    position = add_expression(*expr_ptr);
   } else if (auto expr_ptr =
                  dynamic_cast<const frontend::grouping_expression*>(&expr))
   {
-    return add_expression(*expr_ptr);
+    position = add_expression(*expr_ptr);
   } else if (auto expr_ptr =
                  dynamic_cast<const frontend::number_expression*>(&expr))
   {
-    return add_expression(*expr_ptr);
+    position = add_expression(*expr_ptr);
   } else if (auto expr_ptr =
                  dynamic_cast<const frontend::variable_expression*>(&expr))
   {
-    return add_expression(*expr_ptr);
+    position = add_expression(*expr_ptr);
   } else if (auto expr_ptr =
                  dynamic_cast<const frontend::unary_expression*>(&expr))
   {
-    return add_expression(*expr_ptr);
+    position = add_expression(*expr_ptr);
+  } else {
+    position = -1;  // unreachable;
   }
-  return -1;
+  data.out_index = data.control_flow_index - 1;
+  return position;
 };
 
 ssa_position control_flow_builder::add_expression(
     const frontend::number_expression& expr)
 {
+  auto value = expr.get_value();
   data.uses[data.control_flow_index] = {};
-  data.defines[data.control_flow_index] = expr.get_value();
-  return data.control_flow_index++;
+  auto it =
+      std::find_if(data.defines.begin(),
+                   data.defines.end(),
+                   [&](const auto& pair) { return pair.second == value; });
+  if (it != data.defines.end()) {
+    return it->first;
+  } else {
+    data.defines[data.control_flow_index] = value;
+    return data.control_flow_index++;
+  }
 }
 
 ssa_position control_flow_builder::add_expression(
@@ -85,7 +98,7 @@ ssa_position control_flow_builder::add_expression(
     throw control_flow_exception("Not substract unary op");
   }
   const auto b_expr =
-      expression(position, expression_op::subtract, MINUS_ONE_SSA_POSITION);
+      expression(position, expression_op::multiply, MINUS_ONE_SSA_POSITION);
   data.expressions.insert(std::make_pair(data.control_flow_index, b_expr));
   data.uses[position].insert(data.control_flow_index);
   data.uses[MINUS_ONE_SSA_POSITION].insert(data.control_flow_index);
@@ -99,41 +112,106 @@ ssa_position control_flow_builder::add_expression(
   return add_expression(*expr.get_expr());
 }
 
-void control_flow_builder::optimize()
+void control_flow_builder::replace_position(ssa_position old_position,
+                                            ssa_position new_position)
 {
-  // Copy propagation
-  // Dead code elimination – If exist a block, when one of values is equal zero
-  // – remove the branch
-  // Constant propagation
-  // Defragmentation of indexes
+  for (auto dependency_pos : data.uses.at(old_position)) {
+    data.expressions.at(dependency_pos)
+        .replace_position(old_position, new_position);
+  }
+  data.uses.erase(old_position);
+  if (data.out_index == old_position) {
+    data.out_index = new_position;
+  }
+}
+
+void control_flow_builder::copy_propagation()
+{
+  std::map<backend::expression, ssa_position> expressions_using = {};
+  for (auto pair : data.expressions) {
+    if (!expressions_using.contains(pair.second)) {
+      expressions_using[pair.second] = pair.first;
+    } else {
+      replace_position(pair.first, expressions_using[pair.second]);
+    }
+  }
+}
+
+void control_flow_builder::algebraic_simplification()
+{
+  for (auto& [pos, expr] : data.expressions) {
+    if ((expr.m_operator == expression_op::multiply
+         && expr.is_one_of_positions_equal(ZERO_SSA_POSITION))  // x * 0 = 0
+        || (expr.m_operator == expression_op::subtract
+            && expr.m_left == expr.m_right))  // x - x = 0
+    {
+      replace_position(pos, ZERO_SSA_POSITION);
+    } else if ((expr.m_operator == expression_op::multiply
+                && expr.is_one_of_positions_equal(
+                    ONE_SSA_POSITION))  // x * 1 = x
+               || (expr.m_operator == expression_op::divide
+                   && expr.is_one_of_positions_equal(
+                       ONE_SSA_POSITION))  // x / 1 = x
+               || (expr.m_operator == expression_op::add
+                   && expr.is_one_of_positions_equal(
+                       ZERO_SSA_POSITION))  // x + 0 = x
+               || (expr.m_operator == expression_op::subtract
+                   && expr.is_one_of_positions_equal(
+                       ZERO_SSA_POSITION)))  // x - 0 = x
+    {
+      auto x_position =
+          (expr.m_left == 0 || expr.m_left == 1) ? expr.m_right : expr.m_left;
+      replace_position(pos, x_position);
+    }
+  }
+}
+
+void control_flow_builder::dead_code_elimination()
+{
+  std::set<ssa_position> marked_positions = {};
+  std::vector<ssa_position> worked_positions = {
+      data.out_index,
+  };
+  while (!worked_positions.empty()) {
+    auto pos = worked_positions.back();
+    worked_positions.pop_back();
+    marked_positions.insert(pos);
+    if (data.expressions.contains(pos)) {
+      auto expr = data.expressions.at(pos);
+      worked_positions.push_back(expr.m_left);
+      worked_positions.push_back(expr.m_right);
+    }
+  }
+  for (int i = 0; i < data.control_flow_index; i++) {
+    if (marked_positions.contains(i)) {
+      continue;
+    }
+    if (data.expressions.contains(i)) {
+      auto expr = data.expressions.at(i);
+      data.uses[expr.m_left].erase(i);
+      data.uses[expr.m_right].erase(i);
+      data.expressions.erase(i);
+    }
+    data.variables.erase(i);
+    data.defines.erase(i);
+    data.uses.erase(i);
+  }
 }
 
 void control_flow_builder::execute()
 {
-  // Request variables && Execute by steps
-  std::cout << "Uses:" << std::endl;
-  for (const auto& pair : data.uses) {
-    std::cout << pair.first << ": {";
-    for (auto item : pair.second) {
-      std::cout << item << ", ";
-    }
-    std::cout << "}" << std::endl;
-  }
-  std::cout << "Expressions:" << std::endl;
-  for (const auto& pair : data.expressions) {
-    std::cout << pair.first << ": " << pair.second.m_left
-              << std::to_string(pair.second.m_operator) << pair.second.m_right
-              << std::endl;
-  }
-  std::cout << "Defines:" << std::endl;
   for (const auto& pair : data.defines) {
-    std::cout << pair.first << ": " << pair.second << std::endl;
+    std::cout << "%" << pair.first << " = " << pair.second << std::endl;
   }
-  std::cout << "Variables:" << std::endl;
   for (const auto& pair : data.variables) {
-    std::cout << pair.first << ": " << pair.second << std::endl;
+    std::cout << "%" << pair.first << " = " << pair.second << std::endl;
   }
-  std::cout << "index: " << std::to_string(data.control_flow_index)
-            << std::endl;
+  for (const auto& pair : data.expressions) {
+    std::cout << "%" << pair.first << " = " << "%" << pair.second.m_left << " "
+              << expression_op_to_string(pair.second.m_operator) << " "
+              << "%" << pair.second.m_right << std::endl;
+  }
+
+  std::cout << "Out index: " << std::to_string(data.out_index) << std::endl;
 }
 }  // namespace backend
--- source/backend/control_flow_builder.h
+++ source/backend/control_flow_builder.h
@@ -7,8 +7,6 @@
 
 #include "frontend/parsing/expression.h"
 
-#define MINUS_ONE_SSA_POSITION 0
-
 typedef unsigned long ssa_position;
 
 namespace backend
@@ -22,9 +20,16 @@ enum expression_op
   multiply,
 };
 
+static char expression_op_to_string(expression_op op)
+{
+  static const std::array<const char, 4> mapper = {'+', '-', '/', '*'};
+  return mapper[static_cast<int>(op)];
+}
+
 class expression
 
 {
+public:
   expression(ssa_position left, expression_op op, ssa_position right)
       : m_left(left)
       , m_operator(op)
@@ -32,7 +37,28 @@ class expression
   {
   }
 
+  constexpr auto operator<=>(const expression& other) const
+  {
+    return std::tie(m_left, m_operator, m_right)
+        <=> std::tie(other.m_left, other.m_operator, other.m_right);
+  }
+
 private:
+  void replace_position(ssa_position old_pos, ssa_position new_pos)
+  {
+    if (m_left == old_pos) {
+      m_left = new_pos;
+    }
+    if (m_right == old_pos) {
+      m_right = new_pos;
+    }
+  }
+
+  bool is_one_of_positions_equal(ssa_position pos)
+  {
+    return (m_left == pos) || (m_right == pos);
+  }
+
   ssa_position m_left;
   expression_op m_operator;
   ssa_position m_right;
@@ -45,28 +71,46 @@ class control_flow_builder
   struct control_flow_data
   {
     std::map<ssa_position, double> defines = {
+#define MINUS_ONE_SSA_POSITION 0
         {MINUS_ONE_SSA_POSITION, -1},
-    };  // %1 - число
-    std::map<ssa_position, std::string> variables;  // %1 - название переменной
-    std::map<ssa_position, backend::expression> expressions;  // %1 = %2 + %3
-    std::map<ssa_position, std::set<ssa_position>>
-        uses;  // %1 и где используется
-    ssa_position control_flow_index = 1;
+#define ZERO_SSA_POSITION 1
+        {ZERO_SSA_POSITION, 0},
+#define ONE_SSA_POSITION 2
+        {ONE_SSA_POSITION, 1},
+    };
+    std::map<ssa_position, std::string> variables;
+    std::map<ssa_position, backend::expression> expressions;
+    std::map<ssa_position, std::set<ssa_position>> uses;
+
+    ssa_position control_flow_index = 3;
+    ssa_position out_index;
   };
 
   control_flow_data data;
 
-public:
-  explicit control_flow_builder() {}
-
-  ssa_position add_expression(const frontend::expression& expr);
   ssa_position add_expression(const frontend::binary_expression& expr);
   ssa_position add_expression(const frontend::grouping_expression& expr);
   ssa_position add_expression(const frontend::number_expression& expr);
   ssa_position add_expression(const frontend::variable_expression& expr);
   ssa_position add_expression(const frontend::unary_expression& expr);
 
-  void optimize();
+  void replace_position(ssa_position old_position, ssa_position new_position);
+
+  void copy_propagation();
+  void algebraic_simplification();
+  void dead_code_elimination();
+
+public:
+  explicit control_flow_builder() {}
+
+  ssa_position add_expression(const frontend::expression& expr);
+
+  void optimize()
+  {
+    copy_propagation();
+    algebraic_simplification();
+    dead_code_elimination();
+  }
 
   void execute();
 };
